f5c1cc40be6abc649e9a5db5bb00128334e246e6
Jordan Cline <jdcline04@gmail.com>
2020-05-02 22:46:50 -0600

    Initial one-page website version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TitleBar from './TitleBar.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-     <h1>Mrs. Quizzlepuff Gives you Her Regards</h1>
+     <TitleBar />
+     <button className="nav-button">Create Quiz</button>
+     <br />
+     <button className="nav-button">Look-up Results</button>
+     <footer className="page-footer">
+      <p>Contact Us</p>
+      <p>Privacy Policy</p>
+      <p>Copyright &#169; One Quiz 2020 All Rights Reserved</p> 
+    </footer> 
     </div>
   );
 }
---
+++ src/TitleBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TitleBar = () => {
+    const title = "Welcome to One Quiz";
+    return (
+        <div className="main-title-bar">
+            <h1 className="main-title-text">{title}</h1>
+        </div>
+    )
+}
+
+export default TitleBar;
